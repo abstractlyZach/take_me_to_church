7f2c0816025ac777e482b9c7738ae7809b065b6c
Zach Lee <zach3lee@gmail.com>
2017-02-16 18:53:34 -0800

    finished basic logic for seeing if people are coming to church

--- check_rides.py
+++ check_rides.py
@@ -17,3 +17,53 @@ def coming_to_church(date, name):
 	worksheet = spreadsheet.worksheet('Coming {}'.format(date))
 	name_column = get_info.get_column_index('name', worksheet)
 	return name in worksheet.col_values(name_column)
+
+def person_field_missing(date, name, field):
+	'Returns true if the field has "unknown" in it'
+	assert coming_to_church(date, name)
+	spreadsheet = gspread_connection.connect()
+	worksheet = spreadsheet.worksheet('Coming {}'.format(date))
+	field_column = get_info.get_column_index(field, worksheet)
+	person_row = get_person_row(date, name)
+	return worksheet.cell(person_row, field_column).value == 'unknown'
+
+def set_as_driver(date, name):
+	'Sets a person as driver for the date'
+	assert coming_to_church(date, name)
+	spreadsheet = gspread_connection.connect()
+	worksheet = spreadsheet.worksheet('Coming {}'.format(date))
+	field_column = get_info.get_column_index('driver/rider', worksheet)
+	person_row = get_person_row(date, name)
+	worksheet.update_cell(person_row, field_column, 'driver')
+
+def set_as_rider(date, name):
+	'Sets a person as rider for the date'
+	assert coming_to_church(date, name)
+	spreadsheet = gspread_connection.connect()
+	worksheet = spreadsheet.worksheet('Coming {}'.format(date))
+	field_column = get_info.get_column_index('driver/rider', worksheet)
+	person_row = get_person_row(date, name)
+	worksheet.update_cell(person_row, field_column, 'rider')
+
+def set_default_departure_time(date, name):
+	set_departure_time(date, name, 'default')
+
+def set_departure_time(date, name, time):
+	assert coming_to_church(date, name)
+	spreadsheet = gspread_connection.connect()
+	worksheet = spreadsheet.worksheet('Coming {}'.format(date))
+	time_column = get_info.get_column_index('departure time', worksheet)
+	person_row = get_person_row(date, name)
+	worksheet.update_cell(person_row, time_column, time)
+
+def get_person_row(date, name):
+	"Gets the row for a person. Returns -1 if they're not coming this week"
+	if not coming_to_church(date, name):
+		return -1
+	else:
+		spreadsheet = gspread_connection.connect()
+		worksheet = spreadsheet.worksheet('Coming {}'.format(date))
+		name_column = get_info.get_column_index('name', worksheet)
+		for row, person_name in enumerate(worksheet.col_values(name_column), 1):
+			if name == person_name:
+				return row
--- handle_message.py
+++ handle_message.py
@@ -7,6 +7,9 @@ import jokes
 import check_rides
 
 DEFAULT_DEPARTURE_TIME = "7:00PM"
+DATE = '2017-02-17'
+DEPARTURE_TIME_MESSAGE = "Are you down to leave at the usual time ({})? " + \
+	"If not, when do you want to leave?"
 
 nltk.download('punkt')
 
@@ -28,29 +31,59 @@ class MessageHandler:
 		'''
 		tokens = word_tokenize(self._message)
 		tokens = [token.lower() for token in tokens]
+		person = get_info.get_person_by_phone(self._phone_number)
 		if self._message.strip().lower() == 'password':
 			return "Welcome to the club"
 		elif self._message.strip().lower() == 'chuck':
 			return jokes.get_chuck_norris_joke()
 		elif tokens[0] == "yes":
-			person = get_info.get_person_by_phone(self._phone_number)
-			if not check_rides.coming_to_church('2017-02-17', person.get_name()):
+			if not check_rides.coming_to_church(DATE, person.get_name()):
 				check_rides.create_person(person.get_name())
+				return "Got it! Are you down to drive people? (yes/no)"
 			else:
-				print("duplicate response!")
+				if check_rides.person_field_missing(DATE, person.get_name(), 'driver/rider'):
+					check_rides.set_as_driver(DATE, person.get_name())
+					return DEPARTURE_TIME_MESSAGE
+				elif check_rides.person_field_missing(DATE, person.get_name(), 'departure time'):
+					check_rides.set_default_departure_time(DATE, person.get_name())
+					return "OK! you'll be leaving at the default time."
+				else:
+					self.unparsed_response()
+					return ""
+
 		elif tokens[0] == "no":
-			pass
-		else: # didn't send the secret password
-			sender = get_info.get_person_by_phone(self._phone_number)
-			if sender == None:
-				name = "stranger"
+			if not check_rides.coming_to_church(DATE, person.get_name()):
+				return "ok! Have a good week"
 			else:
-				name = sender.get_name()
-			format_string = ("Hello {}. You have sent {} messages this session. "
-				'You sent "{}". ')
-			# if sender != None:
-			# 	if sender.get_pickup_location() != None:
-			# 		format_string += "I'll pick you up at {}!".format(sender.get_pickup_location())
-			return format_string.format(name, self._session_counter, self._message)
+				if check_rides.person_field_missing(DATE, person.get_name(), 'driver/rider'):
+					check_rides.set_as_rider(DATE, person.get_name())
+					return DEPARTURE_TIME_MESSAGE
+				else:
+					self.unparsed_response()
+					return ""
+
+		else: # didn't send the secret password
+			coming = check_rides.coming_to_church(DATE, person.get_name())
+			if coming:
+				got_driver_rider = check_rides.person_field_missing(DATE, person.get_name(), 'driver/rider')
+			if coming and got_driver_rider:
+				check_rides.set_departure_time(DATE, person.get_name(), self._message)
+				return "Got it! Message me a new time if you need to change your time"
+			else:
+				sender = get_info.get_person_by_phone(self._phone_number)
+				if sender == None:
+					name = "stranger"
+				else:
+					name = sender.get_name()
+				format_string = ("Hello {}. You have sent {} messages this session. "
+					'You sent "{}". ')
+				# if sender != None:
+				# 	if sender.get_pickup_location() != None:
+				# 		format_string += "I'll pick you up at {}!".format(sender.get_pickup_location())
+				return format_string.format(name, self._session_counter, self._message)
 		return "" # temporary placeholder before I add in more logic. makes sure I always return a string
+
+	def unparsed_response(self):
+		print('idk where this should go. printing message:')
+		print(self._message)
 		 
